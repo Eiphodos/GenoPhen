Fixed saving and loading weights from mixed precision pre-training

models/modeling.py
@@ -44,7 +44,21 @@ def build_ft_legacy_model(cfg, tokenizer_geno, train_dataloader, val_dataloader)
                                                   type_vocab_size=1,
                                                   hidden_size=cfg['model']['geno']['hidden_size'])
             if cfg['model']['geno']['use_pretrained']:
-                geno_model = RobertaModel.from_pretrained(cfg['model']['geno']['pretrained_weights'], geno_m_config)
+                if cfg['model']['geno']['pretrained_weights'][-4:] == '.pth':
+                    sd = torch.load(cfg['model']['geno']['pretrained_weights'], map_location='cpu')
+                    model_sd = sd['model']
+                    if "module." in list(model_sd.keys())[0]:
+                        print("Tag 'module.' found in state dict - fixing!")
+                        for key in list(model_sd.keys()):
+                            model_sd[key.replace("module.", "")] = model_sd.pop(key)
+                    if "roberta." in list(model_sd.keys())[0]:
+                        print("Tag 'roberta.' found in state dict - fixing!")
+                        for key in list(model_sd.keys()):
+                            model_sd[key.replace("roberta.", "")] = model_sd.pop(key)
+                    geno_model = RobertaModel(geno_m_config)
+                    geno_model.load_state_dict(model_sd, strict=False)
+                else:
+                    geno_model = RobertaModel.from_pretrained(cfg['model']['geno']['pretrained_weights'], geno_m_config)
             else:
                 geno_model = RobertaModel(geno_m_config)
         elif cfg['model']['geno']['class'] == 'RobertaHierModel':
@@ -57,7 +71,21 @@ def build_ft_legacy_model(cfg, tokenizer_geno, train_dataloader, val_dataloader)
                                      hidden_size=cfg['model']['geno']['hidden_size'])
             emb = RobertaHierarchicalEmbeddingsV1(config=geno_m_config)
             if cfg['model']['geno']['use_pretrained']:
-                geno_model = RobertaHierModel.from_pretrained(cfg['model']['geno']['pretrained_weights'], geno_m_config)
+                if cfg['model']['geno']['pretrained_weights'][-4:] == '.pth':
+                    sd = torch.load(cfg['model']['geno']['pretrained_weights'], map_location='cpu')
+                    model_sd = sd['model']
+                    if "module." in list(model_sd.keys())[0]:
+                        print("Tag 'module.' found in state dict - fixing!")
+                        for key in list(model_sd.keys()):
+                            model_sd[key.replace("module.", "")] = model_sd.pop(key)
+                    if "roberta." in list(model_sd.keys())[0]:
+                        print("Tag 'roberta.' found in state dict - fixing!")
+                        for key in list(model_sd.keys()):
+                            model_sd[key.replace("roberta.", "")] = model_sd.pop(key)
+                    geno_model = RobertaHierModel(geno_m_config, emb)
+                    geno_model.load_state_dict(model_sd, strict=False)
+                else:
+                    geno_model = RobertaHierModel.from_pretrained(cfg['model']['geno']['pretrained_weights'], geno_m_config)
             else:
                 geno_model = RobertaHierModel(geno_m_config, emb)
 

pretraining.py
@@ -139,10 +139,20 @@ def main(args):
                     if ve_acc > best_val_metric:
                         best_val_metric = ve_acc
                         if dist_misc.is_main_process():
+                            to_save = {
+                                'optimizer': optimizer.state_dict(),
+                                'scaler': scaler.state_dict(),
+                                'scheduler': scheduler.state_dict(),
+                                'epoch': e
+                            }
                             if dist_misc.is_dist_avail_and_initialized():
-                                model.module.save_pretrained(cfg['log_dir'])
+                                # model.module.save_pretrained(cfg['log_dir'])
+                                to_save['model'] = model.module.state_dict()
+
                             else:
-                                model.save_pretrained(cfg['log_dir'])
+                                # model.save_pretrained(cfg['log_dir'])
+                                to_save['model'] = model.state_dict()
+                            torch.save(to_save, os.path.join(cfg['log_dir'], 'best_model_epoch_{}.pth'.format(e)))
                             tokenizer.save_pretrained(cfg['log_dir'])
                 acc_oskar = val_acc_tot / val_acc_n
                 if args.wandb_logging:
@@ -157,10 +167,20 @@ def main(args):
                 dist.barrier()
 
     if dist_misc.is_main_process():
+        to_save = {
+            'optimizer': optimizer.state_dict(),
+            'scaler': scaler.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'epoch': e
+        }
         if dist_misc.is_dist_avail_and_initialized():
-            model.module.save_pretrained(cfg['log_dir'])
+            #model.module.save_pretrained(cfg['log_dir'])
+            to_save['model'] = model.module.state_dict()
+
         else:
-            model.save_pretrained(cfg['log_dir'])
+            #model.save_pretrained(cfg['log_dir'])
+            to_save['model'] = model.state_dict()
+        torch.save(to_save, os.path.join(cfg['log_dir'], 'final_model.pth'))
         tokenizer.save_pretrained(cfg['log_dir'])
 
 if __name__ == '__main__':